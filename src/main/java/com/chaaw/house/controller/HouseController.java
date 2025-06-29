c535c9e5b30def44042683bf032aeeccc28522cf
puan <puanputrisaqinah@gmail.com>
2025-06-29 16:51:43 +0700

    update house service

--- src/main/java/com/chaaw/house/controller/HouseController.java
+++ src/main/java/com/chaaw/house/controller/HouseController.java
@@ -3,10 +3,7 @@ package com.chaaw.house.controller;
 import com.chaaw.house.model.House;
 import com.chaaw.house.service.HouseService;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -22,6 +19,35 @@ public class HouseController {
     @GET
     public Response getHouses() {
         List<House> houses = houseService.getHouses();
-        return Response.status(Response.Status.OK).entity("Success Bele!").build();
+        return Response.status(Response.Status.OK).entity(houses).build();
     }
+
+    @POST
+    public Response addHouse(House kjbhuj) {
+        houseService.addHouse(kjbhuj);
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    @Path("/{houseId}")
+    public Response updateHouse(@PathParam("houseId") String houseId, House bebas) {
+        houseService.updateHouse(houseId, bebas);
+        return Response.status(Response.Status.ACCEPTED).build();
+    }
+
+    @DELETE
+    @Path("/{houseId}")
+    public Response deleteHouse(@PathParam("houseId") String houseId) {
+        houseService.deleteHouse(houseId);
+        return Response.status(Response.Status.ACCEPTED).entity("SUCCESS BRE").build();
+    }
+
+    @GET
+    @Path("/{houseId}")
+    public Response getHouse(@PathParam("houseId") String houseId) {
+        House house = houseService.getHouse(houseId);
+        return Response.status(Response.Status.ACCEPTED).entity(house).build();
+    }
+
+
 }
--- src/main/java/com/chaaw/house/service/HouseService.java
+++ src/main/java/com/chaaw/house/service/HouseService.java
@@ -4,6 +4,7 @@ import com.chaaw.house.model.House;
 import com.chaaw.house.repository.HouseRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 
@@ -14,5 +15,26 @@ public class HouseService {
 
     public List<House> getHouses() {
         return houseRepository.listAll();
+    }
+
+    @Transactional
+    public void addHouse(House kjbhuj) {
+        houseRepository.persist(kjbhuj);
+    }
+
+    @Transactional
+    public void updateHouse(String houseId, House bebas) {
+        House house = houseRepository.findById(houseId);
+        house.setName(bebas.getName());
+        houseRepository.persist(house);
+    }
+
+    @Transactional
+    public void deleteHouse(String houseId) {
+        houseRepository.deleteById(houseId);
+    }
+
+    public House getHouse(String houseId) {
+        return houseRepository.findById(houseId);
     }
 }
